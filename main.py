40e88e0f52ffc1befc24a1f1231c72e94ec6aacc
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-09 18:58:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 # Python entry point for main
+import os
+
+def run_bash(file, func):
+	bashfile = os.path.dirname(__file__) + "/bash/" + file + ".sh"
+	cmd = "source " + bashfile + "; " + func
+	os.system(cmd)
+	input("Command done, press enter to continue")
 
 def get_selection(prompt):
 	usropt = input(prompt)
@@ -8,6 +15,7 @@ def main():
 	input("Python main start")
 	usrsel = get_selection("Test prompt: ")
 	print("Selection = " + usrsel)
+	run_bash(main, show_vars)
 	input("Python main end")
 
 if __name__ == "__main__":
