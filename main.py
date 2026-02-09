aabcfd2d7a11dc8f926ec2b0f048e37ec623cd6a
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-09 18:23:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-# Python entry point
+# Python entry point for main
 
+def get_selection(prompt):
+	usropt = input(prompt)
+	
 def main():
-	input("Python main")
+	input("Python main start")
+	usrsel = get_selection("Test prompt")
+	print("Selection = " + usrsel)
+	input("Python main end")
 
 if __name__ == "__main__":
 	main()
