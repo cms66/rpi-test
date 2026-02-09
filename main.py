cbf76f74e49441b373830911a37f38cb6d11677c
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-09 18:41:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def get_selection(prompt):
 	
 def main():
 	input("Python main start")
-	usrsel = get_selection("Test prompt")
+	usrsel = get_selection("Test prompt: ")
 	print("Selection = " + usrsel)
 	input("Python main end")
 
