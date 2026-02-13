44cd8e53ee37634a5a19e692a0de7e98f5d1950a
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-13 17:15:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,6 @@
 import os
 import python.functions as pf
 import python.vars as pv
-
-#bashfile = "/home/multipi/rpi-test/bash/main.sh"
-
-#def run_bash(func):
-#	strcmd = "source " + pv.bashfile + "; source_bash; " + func
-#	os.system(strcmd)
-#	input("Command done, press enter to continue")
 	
 def main():
 	input("Python main start")
