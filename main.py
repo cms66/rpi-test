848827a7bc9caaa57e6e8311acbc2f14abbef54c
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-13 16:59:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import python.vars as pv
 
 #bashfile = "/home/multipi/rpi-test/bash/main.sh"
 
-def run_bash(func):
-	strcmd = "source " + pv.bashfile + "; source_bash; " + func
-	os.system(strcmd)
-	input("Command done, press enter to continue")
+#def run_bash(func):
+#	strcmd = "source " + pv.bashfile + "; source_bash; " + func
+#	os.system(strcmd)
+#	input("Command done, press enter to continue")
 	
 def main():
 	input("Python main start")
-	run_bash('show_vars; test_func')
+	pf.run_bash('show_vars; test_func')
 	input("Python main end")
 
 if __name__ == "__main__":
